546d4959b06a8299feb76f3b8aae84e13449d2e8
imvenx <imvenx@proton.me>
2024-08-15 22:34:11 +0200

    update name to arcanejam2

--- quasar.config.js
+++ quasar.config.js
@@ -60,7 +60,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      publicPath: '/arcanejam/',
+      publicPath: '/arcanejam2/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
